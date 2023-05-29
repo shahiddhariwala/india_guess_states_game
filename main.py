2cb047cd9c90d483cfb7cec0a68bb6adb736e201
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-29 23:19:23 +0530

    Map as bg for screen added

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello Inaaya")
+import turtle
+
+screen = turtle.Screen()
+screen.title("Guess india state")
+map_img = "india_states.gif"
+screen.addshape(map_img)
+turtle.shape(map_img)
+turtle.mainloop()
