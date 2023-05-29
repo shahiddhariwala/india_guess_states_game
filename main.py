2ae33b4868c4a5dfe5f5b9ac6d0caf9ee65af2d7
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-29 23:52:23 +0530

    Writing on Map logic added

--- main.py
+++ main.py
@@ -2,14 +2,32 @@ import turtle
 import pandas
 
 screen = turtle.Screen()
-screen.title("Guess india state")
+screen.title("Guess indian state")
 map_img = "india_states.gif"
 screen.addshape(map_img)
 turtle.shape(map_img)
 
 state_df = pandas.read_csv("state_cords.csv")
 guessed_states = []
-user_input = screen.textinput(f"Guess states {len(guessed_states)}/{state_df.size}", "Please type the name of state in india`")
+states_name = list(state_df.state)
+
+while len(guessed_states) != state_df["state"].count():
+    user_input = screen.textinput(f"Guess states {len(guessed_states)}/{state_df.state.count()}",
+                                  "Please type the name of state in india`").title()
+    if user_input == "Exit":
+        break
+
+    if user_input in states_name:
+        state_data = state_df[state_df.state == user_input]
+        state_turtle = turtle.Turtle()
+        state_turtle.penup()
+        state_turtle.color("red")
+        state_turtle.goto(int(state_data.x), int(state_data.y))
+        state_turtle.hideturtle()
+        state_turtle.write(f"{user_input}", False, "center", ("Fira-sans", 18, "normal"))
+        guessed_states.append(user_input)
+
+
 
 # Function to get the coordinates of a click
 # def get_coordinates(x, y):
