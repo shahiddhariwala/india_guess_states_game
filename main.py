f1a879494cb85825b47be223e69f4745b08c3321
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-30 00:24:39 +0530

    COnverting Dataframe to CSV and list compression implemented

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ while len(guessed_states) != state_df["state"].count():
     user_input = screen.textinput(f"Guess states {len(guessed_states)}/{state_df.state.count()}",
                                   "Please type the name of state in india`").title()
     if user_input == "Exit":
+        missing_states = [state for state in states_name if state not in guessed_states]
+        missing_df = pandas.DataFrame(missing_states)
+        missing_df.to_csv("Missing_states.csv")
         break
 
     if user_input in states_name:
@@ -27,10 +30,7 @@ while len(guessed_states) != state_df["state"].count():
         state_turtle.write(f"{user_input}", False, "center", ("Fira-sans", 18, "normal"))
         guessed_states.append(user_input)
 
-with open("missed_state.txt", "w") as data:
-    for state in states_name:
-        if state not in guessed_states:
-            data.writelines(f"{state}\nMis")
+
 
 # Function to get the coordinates of a click
 # def get_coordinates(x, y):
